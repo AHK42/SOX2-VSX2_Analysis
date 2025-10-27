8ceec715dc1461ba3b0a5874e29dff3e1abfc6c0
Abdullah <ahk42@pitt.edu>
2025-10-27 17:14:28 -0400

    update paths

--- Fig1/Fig1_deepTools.sh
+++ Fig1/Fig1_deepTools.sh
@@ -7,82 +7,82 @@ module load python/anaconda3.10-2022.10
 source activate deeptools
 
 # Define constants
-BLACKLIST="/bgfs/ialdiri/Genomes/mm10-blacklist.v2.bed.gz"
+BLACKLIST="/ix1/ialdiri/Genomes/mm10-blacklist.v2.bed.gz"
 CHROM_SIZE="2650000000"
 WINDOW_SIZE=10
 
 # Define Paths for Sox2CR
-SOX2_BAM_DIR="/bgfs/ialdiri/CR-ChIP/nfcore_runs/Sox2CR/results/02_alignment/bowtie2/target/dedup"
-SOX2_BIGWIG_DIR="/bgfs/ialdiri/CR-ChIP/bamCovBW/SOX2"
+SOX2_BAM_DIR="/ix1/ialdiri/CR-ChIP/nfcore_runs/Sox2CR/results/02_alignment/bowtie2/target/dedup"
+SOX2_BIGWIG_DIR="/ix1/ialdiri/CR-ChIP/bamCovBW/SOX2"
 SOX2_BAM_FILES=("SOX2_S1_R1.target.dedup.sorted.bam")
 
 mkdir -p $SOX2_BIGWIG_DIR
 
-# Loop over BAM files to generate bigWig files
-for BAM in "${SOX2_BAM_FILES[@]}"; do
-   SAMPLE_NAME=$(basename "$BAM" | cut -d. -f1)
-   bamCoverage \
-       --bam "$SOX2_BAM_DIR/$BAM" \
-       --outFileName "$SOX2_BIGWIG_DIR/${SAMPLE_NAME}.bigWig" \
-       --binSize $WINDOW_SIZE \
-       --normalizeUsing RPGC \
-       --effectiveGenomeSize $CHROM_SIZE \
-       --ignoreForNormalization chrX \
-       --blackListFileName $BLACKLIST \
-       --numberOfProcessors max \
-       --verbose \
-       --extendReads  # Specific to C&R
-done
+# # Loop over BAM files to generate bigWig files
+# for BAM in "${SOX2_BAM_FILES[@]}"; do
+#    SAMPLE_NAME=$(basename "$BAM" | cut -d. -f1)
+#    bamCoverage \
+#        --bam "$SOX2_BAM_DIR/$BAM" \
+#        --outFileName "$SOX2_BIGWIG_DIR/${SAMPLE_NAME}.bigWig" \
+#        --binSize $WINDOW_SIZE \
+#        --normalizeUsing RPGC \
+#        --effectiveGenomeSize $CHROM_SIZE \
+#        --ignoreForNormalization chrX \
+#        --blackListFileName $BLACKLIST \
+#        --numberOfProcessors max \
+#        --verbose \
+#        --extendReads  # Specific to C&R
+# done
 
-# Define paths for SOX2 ATAC Seq 
-ATAC_BAM_DIR="/bgfs/ialdiri/ATAC-Seq/Sox2_ATAC-Seq/outDir/bowtie2/merged_library"
-ATAC_BIGWIG_DIR="/bgfs/ialdiri/ATAC-Seq/Sox2_ATAC-Seq/bamCovBW"
-BAM_FILES=("WT1_REP1.mLb.clN.sorted.bam" )
+# # Define paths for SOX2 ATAC Seq 
+# ATAC_BAM_DIR="/ix1/ialdiri/ATAC-Seq/Sox2_ATAC-Seq/outDir/bowtie2/merged_library"
+# ATAC_BIGWIG_DIR="/ix1/ialdiri/ATAC-Seq/Sox2_ATAC-Seq/bamCovBW"
+# BAM_FILES=("WT1_REP1.mLb.clN.sorted.bam" )
 
-mkdir -p $ATAC_BIGWIG_DIR
+# mkdir -p $ATAC_BIGWIG_DIR
 
-Generate bigWig Files
-for BAM in "${BAM_FILES[@]}"; do
-    SAMPLE_NAME=$(basename "$BAM" | cut -d. -f1)
-    bamCoverage \
-        --bam "$ATAC_BAM_DIR/$BAM" \
-        --outFileName "$ATAC_BIGWIG_DIR/${SAMPLE_NAME}.bigWig" \
-        --binSize $WINDOW_SIZE \
-        --normalizeUsing RPGC \
-        --effectiveGenomeSize $CHROM_SIZE \
-        --ignoreForNormalization chrX \
-        --blackListFileName $BLACKLIST \
-        --numberOfProcessors max \
-        --verbose 
-done
+# Generate bigWig Files
+# for BAM in "${BAM_FILES[@]}"; do
+#     SAMPLE_NAME=$(basename "$BAM" | cut -d. -f1)
+#     bamCoverage \
+#         --bam "$ATAC_BAM_DIR/$BAM" \
+#         --outFileName "$ATAC_BIGWIG_DIR/${SAMPLE_NAME}.bigWig" \
+#         --binSize $WINDOW_SIZE \
+#         --normalizeUsing RPGC \
+#         --effectiveGenomeSize $CHROM_SIZE \
+#         --ignoreForNormalization chrX \
+#         --blackListFileName $BLACKLIST \
+#         --numberOfProcessors max \
+#         --verbose 
+# done
 
-BIGWIG_FILES=("$SOX2_BIGWIG_DIR/SOX2_S3_R1.bigWig" "$ATAC_BIGWIG_DIR/WT1_REP1.bigWig")
-PEAKS_DIR="/bgfs/ialdiri/Sox2_ATAC/Peaks"
+# BIGWIG_FILES=("$SOX2_BIGWIG_DIR/SOX2_S3_R1.bigWig" "$ATAC_BIGWIG_DIR/WT1_REP1.bigWig")
+# PEAKS_DIR="/ix1/ialdiri/Sox2_ATAC/Peaks"
 
-# Generate matrix and plots
-computeMatrix reference-point --referencePoint center -b 2000 -a 2000 \
-    -S "${BIGWIG_FILES[@]}" \
-    -R "$PEAKS_DIR/Sox2CR_ATAC_int.bed" \
-    --binSize $WINDOW_SIZE \
-    -o "SOX2CR_ATAC.gz" \
-    --sortRegions descend \
-    --sortUsing mean \
-    --missingDataAsZero \
-    --verbose -p max --skipZeros --smartLabels
+# # Generate matrix and plots
+# computeMatrix reference-point --referencePoint center -b 2000 -a 2000 \
+#     -S "${BIGWIG_FILES[@]}" \
+#     -R "$PEAKS_DIR/Sox2CR_ATAC_int.bed" \
+#     --binSize $WINDOW_SIZE \
+#     -o "SOX2CR_ATAC.gz" \
+#     --sortRegions descend \
+#     --sortUsing mean \
+#     --missingDataAsZero \
+#     --verbose -p max --skipZeros --smartLabels
 
-# Fig 1A
-plotHeatmap -m SOX2CR_ATAC.gz -out SOX2CR_ATAC.png \
-    --colorMap 'Blues' \
-    --verbose \
-    -T "Sox2 CR vs ATAC E14.5" \
-    -x "" \
-    --zMax 25 160 \
-    --averageTypeSummaryPlot mean \
-    --dpi 600 --legendLocation none
+# # Fig 1A
+# plotHeatmap -m SOX2CR_ATAC.gz -out SOX2CR_ATAC.png \
+#     --colorMap 'Blues' \
+#     --verbose \
+#     -T "Sox2 CR vs ATAC E14.5" \
+#     -x "" \
+#     --zMax 25 160 \
+#     --averageTypeSummaryPlot mean \
+#     --dpi 600 --legendLocation none
 
 #Fig 1G 
 BIGWIG_FILES=("$STAGES_BIGWIG_DIR/E14.5_REP1.bigWig" "$STAGES_BIGWIG_DIR/P7_REP1.bigWig" "$STAGES_BIGWIG_DIR/P21_REP1.bigWig")
-PEAKS_DIR="/bgfs/ialdiri/Sox2_ATAC/Peaks" 
+PEAKS_DIR="/ix1/ialdiri/Sox2_ATAC/Peaks" 
 
 computeMatrix reference-point --referencePoint center -b 2000 -a 2000 \
     -S "${BIGWIG_FILES[@]}" \
--- Sup/Sup.sh
+++ Sup/Sup.sh
@@ -24,7 +24,9 @@ WINDOW_SIZE=10
 
 # Define BAM Paths 
 
-IGG="/bgfs/ialdiri/CR-ChIP/nfcore_runs/CR/Sox2CR_IgG/outDir/02_alignment/bowtie2/target/dedup/IgG_E14.5_R1.target.dedup.sorted.bam"
+# IGG="/bgfs/ialdiri/CR-ChIP/nfcore_runs/CR/Sox2CR_IgG/outDir/02_alignment/bowtie2/target/dedup/IgG_E14.5_R2.target.dedup.sorted.bam"
+# CHIP="/bgfs/ialdiri/CR-ChIP/nfcore_runs/ChIP/SOX2-PAX6_ChIP/outDir/bowtie2/mergedLibrary/SOX2.mLb.clN.sorted.bam"
+CHIP_INPUT="/bgfs/ialdiri/CR-ChIP/nfcore_runs/ChIP/SOX2-PAX6_ChIP/outDir/bowtie2/mergedLibrary/Input.mLb.clN.sorted.bam"
 
 BW_DIR="/bgfs/ialdiri/Sox2_Vsx2_Original_Analysis/Sox2-VSX2_Analysis/bamCovBW"
 
@@ -32,8 +34,8 @@ mkdir -p $BW_DIR
 
 # # Generate bigWig files for IgG only (previous BWs already generated)
 #     bamCoverage \
-#         --bam "$IGG" \
-#         --outFileName "$BW_DIR/E14.5_IGG_REP1.bigWig" \
+#         --bam "$CHIP_INPUT" \
+#         --outFileName "$BW_DIR/SOX2_ChIP_Input.bigWig" \
 #         --binSize $WINDOW_SIZE \
 #         --normalizeUsing RPGC \
 #         --effectiveGenomeSize $CHROM_SIZE \
@@ -41,14 +43,16 @@ mkdir -p $BW_DIR
 #         --blackListFileName $BLACKLIST \
 #         --numberOfProcessors max \
 #         --verbose\
-#         --extendReads
+#         # --extendReads
 
 # Generate profilePlots for SOX2 
 
 BIGWIG_FILES=("$BW_DIR/SOX2_S1_R1.bigWig" 
-			  "$BW_DIR/WT1_REP1.bigWig"
-			  "$BW_DIR/E14.5_IGG_REP1.bigWig"
-			  "$BW_DIR/SOX2_NB_S2_R1.bigWig"
+			  # "$BW_DIR/WT1_REP1.bigWig"
+			  # "$BW_DIR/E14.5_IGG_REP2.bigWig"
+			  # "$BW_DIR/SOX2_NB_S2_R1.bigWig"
+			  "$BW_DIR/SOX2_ChIP.bigWig"
+			  "$BW_DIR/SOX2_ChIP_Input.bigWig"
 			  )
 
 SOX2_PEAKS="/bgfs/ialdiri/Sox2_Vsx2_Original_Analysis/Peaks/SOX2_consensus.bed"
@@ -56,37 +60,51 @@ SOX2_PEAKS="/bgfs/ialdiri/Sox2_Vsx2_Original_Analysis/Peaks/SOX2_consensus.bed"
 # computeMatrix reference-point --referencePoint center -b 2000 -a 2000 \
 #     -S "${BIGWIG_FILES[@]}" \
 #     -R $SOX2_PEAKS \
-#     -o "Sup_matrix.gz" \
+#     -o "Sox2_cor_matrix.gz" \
 #     --sortRegions descend \
 #     --sortUsing mean \
 #     --missingDataAsZero \
 #     --verbose -p max --skipZeros --smartLabels
 
-# Sup Figure
-plotProfile -m Sup_matrix.gz \
-    -out Sup_profile_line.png \
-    --plotType lines \
-    --dpi 600 \
-	--samplesLabel "SOX2" "E14.5 ATAC" "IgG" "Input" \
-	--regionsLabel "Sox2 C&R Peaks (3498)"
 
-plotProfile -m Sup_matrix.gz \
-    -out Sup_profile_heatmap.png \
-    --plotType heatmap \
-    --dpi 600 \
-    --colors RdBu_r \
-    --yMax 20 150 20 20 \
-    --regionsLabel "Sox2 C&R Peaks (3498)"
+# plotHeatmap -m Sup_matrix.gz -out Sup_TornadoPlot.png \
+#     --colorMap 'Blues' \
+#     --verbose \
+#     -T "" \
+#     -x "" \
+# 	--yMax 25 160 25 25 \
+# 	--zMax 25 160 25 25 \
+# 	--samplesLabel "SOX2" "ATAC" "IgG" "Input" \
+# 	--regionsLabel "Sox2 C&R Peaks (3498)" \
+#     --averageTypeSummaryPlot mean \
+#     --dpi 600 --legendLocation none
 
-plotHeatmap -m Sup_matrix.gz -out Sup_TornadoPlot.png \
+plotHeatmap -m Sox2_cor_matrix.gz -out Sox2_cor_TornadoPlot.png \
     --colorMap 'Blues' \
     --verbose \
     -T "" \
     -x "" \
-	--yMax 25 160 25 25 \
-	--zMax 25 160 25 25 \
-	--samplesLabel "SOX2" "E14.5 ATAC" "IgG" "Input" \
+	--samplesLabel "SOX2 C&R" "SOX2 ChIP" "ChIP Input" \
 	--regionsLabel "Sox2 C&R Peaks (3498)" \
     --averageTypeSummaryPlot mean \
-    --dpi 600 --legendLocation none
+    --dpi 600 --legendLocation none 
+
+plotProfile -m Sox2_cor_matrix.gz \
+    -out Sox2_cor_profilePlot.png \
+    --plotType lines \
+    --dpi 600 \
+	--samplesLabel "SOX2 C&R" "SOX2 ChIP" "ChIP Input" \
+	--regionsLabel "Sox2 C&R Peaks (3498)" \
+	--perGroup
+
+plotProfile -m Sox2_cor_matrix.gz \
+    -out Sox2_cor_Heatmap.png \
+    --plotType heatmap \
+    --dpi 600 \
+    --colors RdBu_r \
+	--samplesLabel "SOX2 C&R" "SOX2 ChIP" "ChIP Input" \
+    --regionsLabel "Sox2 C&R Peaks (3498)" \
+	--perGroup
+
+
 
